c30c5b0ac513e79c5931308c30e6d54d3b0f4dc1
Thunyathon <lostseaway@me.com>
2022-01-30 23:50:27 +0700

    Initial Commit

---
+++ FirebaseToDoList/FirebaseToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseToDoListApp.swift
+//  FirebaseToDoList
+//
+//  Created by Thunyathon  Jaruchotrattanasakul on 30/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
